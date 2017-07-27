0cf3c22d330891e3014c7a50e3167eb7e3b9c85e
Ryan Collins <ryan@ryanmcollins.com>
2017-07-27 16:18:03 -0400

    Initialized Swift Package
    Created a new Swift Project, and did some basic target naming to make sure that Xcode is clear

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Perfect-Test",
+    targets: [
+        Target(name: "Test", dependencies: [])
+    ],
+    dependencies: []
+)
